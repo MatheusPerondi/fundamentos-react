03443be3d611f2689dd27e6b2d8fb22564975dd5
Matheus Perondi <mathsap40@outlook.com>
2025-07-29 21:51:40 -0300

    configura tema

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from "next/app"
 import { Provider } from "@/components/ui/provider"
 
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider>
